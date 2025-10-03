011a37a251cee6d2672c54fd38c29cf8b31812e3
arnavanand2005 <arnavanand2005@gmail.com>
2025-10-03 16:01:56 +0530

    reconfiguring backend

--- api/index.js
+++ api/index.js
@@ -1,19 +1,20 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connected to MongoDB');
-}
-).catch(err => {
-    console.error('MongoDB connection error:', err);
-});
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB");
+    }).catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    }
+);
 
 app.listen(3000, () => {
-    console.log('API server running on http://localhost:3000');
-    });
+  console.log("Server is running on port 3000");
+});
+
 
